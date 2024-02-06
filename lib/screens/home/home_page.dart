bcd69bad72c4dc18f467fab4326064d7e4228bb8
IdaDanuartha <danuart21@gmail.com>
2024-02-06 23:19:39 +0800

    header component page

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -6,13 +6,64 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          "Home Page",
-          style: semiBoldText20,
+    Widget header() {
+      return Container(
+        margin: EdgeInsets.symmetric(horizontal: 30),
+        child: Row(
+          children: [
+            Container(
+              width: 50,
+              height: 50,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: AssetImage('assets/images/profile-pic.png')
+                )
+              ),
+            ),
+            SizedBox(width: 8),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Hello Danuartha,",
+                  style: semiBoldText16,
+                ),
+                Text(
+                  "Good morning",
+                  style: regularText14.copyWith(color: greyColor),
+                )
+              ],
+            ),
+            Spacer(),
+            Image.asset(
+              "assets/icons/icon-menu.png",
+              width: 18,
+            )
+          ],
         ),
-      ),
+      );
+    }
+
+    return Scaffold(
+      body: ListView(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(
+              vertical: 30
+            ),
+            decoration: BoxDecoration(
+              color: whiteColor,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(30)
+              )
+            ),
+            child: Column(children: [
+              header()
+            ],),
+          )
+        ],
+      )      
     );
   }
 }
